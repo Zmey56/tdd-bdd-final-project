f2f4ae8cdf028146f49b3db4cd6358103f55f3a5
AleksTuT <zmey56@gmail.com>
2025-10-01 06:33:41 +0400

    Exercise 3: Create Test Cases and Code for REST API

---
+++ sitecustomize.py
@@ -0,0 +1,7 @@
+import collections
+import collections.abc
+
+collections.Callable = collections.abc.Callable
+collections.Mapping = collections.abc.Mapping
+collections.MutableMapping = collections.abc.MutableMapping
+collections.Sequence = collections.abc.Sequence
